380ddefa8c6a9295812fff53c9235adb9cb858d6
Safayet <mrishafayet003@gmail.com>
2022-07-20 14:05:48 +0600

    middleware added

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 4000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Stooon Fashion Shop!!!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`YaY! Stooon mama choltece ${port}`)
 })
